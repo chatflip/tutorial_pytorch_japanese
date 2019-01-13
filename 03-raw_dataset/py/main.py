cb80e610d60ba0974a636581698e31644af3a04a
Kazushige Okayasu <oo.chat.flip@gmail.com>
2019-01-14 03:13:58 +0900

    fix 03

--- 03-raw_dataset/py/main.py
+++ 03-raw_dataset/py/main.py
@@ -20,54 +20,89 @@ from tensorboardX import SummaryWriter
 from torchvision import datasets, transforms
 from alex import alex
 from loadDB import AnimeFaceDB
+from utils import AverageMeter
 
-def make_dir(save_root,folder_name):
-    if folder_name == "pass":
-        if not os.path.exists(save_root):
-            os.mkdir(save_root)
-    elif not os.path.exists(os.path.join(save_root,folder_name)):
-        os.mkdir(os.path.join(save_root,folder_name))
+#精度出す関数
+def accuracy(output, target, topk=(1,)):
+    """Computes the accuracy over the k top predictions for the specified values of k"""
+    with torch.no_grad():
+        maxk = max(topk)
+        batch_size = target.size(0)
+
+        _, pred = output.topk(maxk, 1, True, True)
+        pred = pred.t()
+        correct = pred.eq(target.view(1, -1).expand_as(pred))
+
+        res = []
+        for k in topk:
+            correct_k = correct[:k].view(-1).float().sum(0, keepdim=True)
+            res.append(correct_k.mul_(100.0 / batch_size))
+        return res
 
 def train(args, model, device, train_loader, writer, optimizer, epoch, iteration):
     #ネットワークを学習用に設定
     #ex.)dropout,batchnormを有効
     model.train()
-    for batch_idx, (data, target) in enumerate(train_loader):
+    #AverageMeterの値の初期化
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    top3 = AverageMeter()
+    end = time.time()#1回目の読み込み時間計測用
+    for i, (data, target) in enumerate(train_loader):
+        data_time.update(time.time() - end)#画像のロード時間記録
         data, target = data.to(device), target.to(device)#gpu使うなら画像とラベルcuda化
         optimizer.zero_grad()#勾配初期化
         output = model(data)#sofmaxm前まで出力(forward)
         loss = nn.CrossEntropyLoss(size_average=True)(output, target)#ネットワークの出力をsoftmax + ラベルとのloss計算
+        acc1, acc3 = accuracy(output, target, topk=(1, 5))#予測した中で1番目と3番目までに正解がある率
+        losses.update(loss.item(), data.size(0))
+        top1.update(acc1[0], data.size(0))
+        top3.update(acc3[0], data.size(0))
         loss.backward()#勾配計算(backprop)
         optimizer.step()#パラメータ更新
+        batch_time.update(time.time() - end)#画像ロードからパラメータ更新にかかった時間記録
+        end = time.time()#基準の時間更新
         #log_intervalごとに進行具合とloss表示
-        if batch_idx % args.log_interval == 0:
-            print("Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
-                epoch, batch_idx * len(data), len(train_loader.dataset),
-                100. * batch_idx / len(train_loader), loss.item()))
-            writer.add_scalars("loss", {"train": loss}, iteration)
+        if i % args.log_interval == 0:
+            print('Epoch: [{0}][{1:5d}/{2:5d}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Acc@1 {top1.val:.3f} ({top1.avg:.3f})\t'
+                  'Acc@3 {top3.val:.3f} ({top3.avg:.3f})'.format(
+                   epoch, i, len(train_loader), batch_time=batch_time,
+                   data_time=data_time, loss=losses, top1=top1, top3=top3))
+            writer.add_scalars("loss", {"train": losses.val}, iteration)
+            writer.add_scalars("accuracy",{"train": top1.val}, iteration)
         iteration += 1
 
 def test(args, model, device, test_loader, writer, iteration):
     #ネットワークを評価用に設定
     #ex.)dropout,batchnormを恒等関数に
     model.eval()
-    test_loss = 0
-    correct = 0
+    #AverageMeterの値の初期化
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
     #勾配計算しない(計算量低減)
     with torch.no_grad():
+        end = time.time()#基準の時間更新
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)#gpu使うなら画像とラベルcuda化
             output = model(data)#sofmaxm前まで出力(forward)#評価データセットでのloss計算
-            test_loss += nn.CrossEntropyLoss(size_average=False)(output, target).item() # sum up batch loss
-            pred = output.max(1, keepdim=True)[1] #softmaxでargmax計算
-            correct += pred.eq(target.view_as(pred)).sum().item()#バッチ内の正解数計算
-    test_loss /= len(test_loader.dataset)#dataset数で割って正解計算
+            loss = nn.CrossEntropyLoss(size_average=False)(output, target) # sum up batch loss
+            losses.update(loss.item(), data.size(0))
+            acc1 = accuracy(output, target)#ラベルと合ってる率を算出
+            top1.update(acc1[0], data.size(0))
+            batch_time.update(time.time() - end)#画像ロードからパラメータ更新にかかった時間記録
+            end = time.time()#基準の時間更新
     #test_loss格納
-    writer.add_scalars("loss", {"test": test_loss}, iteration)
-    writer.add_scalars("accuracy", {"test": 100. * correct / len(test_loader.dataset)}, iteration)
-    print("\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    writer.add_scalars("loss", {"test": losses.avg}, iteration)
+    writer.add_scalars("accuracy", {"test":100.*top1.avg}, iteration)
+
+    print('Test Acc@1 {top1.avg:.4f}\t loss {loss.avg:.4f}\t Time {batch_time.ave:.3f}'.format(top1=top1, loss=losses, batch_time=batch_time))
 
 def opt():
     parser = argparse.ArgumentParser(description="PyTorch AnimeFace")
@@ -86,7 +121,7 @@ def opt():
     parser.add_argument("--no-cuda", action="store_true", default=False, help="disables CUDA training")
     parser.add_argument("--img_size", type = int, default=256, help="image size")
     parser.add_argument("--crop_size", type = int, default=224, help="crop size")
-    parser.add_argument("--num_workers", type=int, default=10, help="num of pallarel threads(dataloader)")
+    parser.add_argument("--num_workers", type=int, default=4, help="num of pallarel threads(dataloader)")
     parser.add_argument("--seed", type=int, default=1, metavar="S", help="random seed (default: 1)")
     parser.add_argument("--log-interval", type=int, default=10, metavar="N", help="how many batches to wait before logging training status")
     args = parser.parse_args()
@@ -94,8 +129,10 @@ def opt():
 
 if __name__ == "__main__":
     args = opt()
-    make_dir("./weight","pass")
-    torch.manual_seed(args.seed)#どこに効いてるか分からないけど乱数テーブル固定
+    #フォルダが存在してなければ作る
+    if not os.path.exists(args.path2weight):
+        os.mkdir(args.path2weight)
+    torch.manual_seed(args.seed)#torchとtorchvisionで使う乱数固定
     use_cuda = not args.no_cuda and torch.cuda.is_available()#gpu使えるか and 使うか
     device = torch.device("cuda" if use_cuda else "cpu")#cpuとgpu自動選択 (pytorch0.4.0以降の書き方)
     writer = SummaryWriter(log_dir="./log/AnimeFace")#tensorboard用のwriter作成
@@ -105,11 +142,11 @@ if __name__ == "__main__":
                                           transforms.RandomCrop((args.crop_size, args.crop_size)),#クロップ
                                           transforms.RandomHorizontalFlip(p=0.5),#左右反転
                                           transforms.ToTensor(),#テンソル化
-                                          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5 ,0.5))#標準化
+                                          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])#標準化
                                           ])
     test_transform = transforms.Compose([transforms.Resize((args.crop_size, args.crop_size), interpolation=5),#リサイズ
                                           transforms.ToTensor(),#テンソル化
-                                          transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5 ,0.5))#標準化
+                                          transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])#標準化
                                           ])
 
     #AnimeFaceの学習用データ設定
@@ -131,17 +168,16 @@ if __name__ == "__main__":
     #学習と評価
     for epoch in range(1, args.epochs + 1):
         scheduler.step()#epoch 0 スタートだから+1して数値を合わせてスケジューラ開始
-        train(args, model, device, train_loader, writer, optimizer, epoch, iteration)
+        #train(args, model, device, train_loader, writer, optimizer, epoch, iteration)
         iteration += len(train_loader)#1epoch終わった時のiterationを足す
         test(args, model, device, test_loader, writer, iteration)
         #重み保存
         if epoch % args.save_interval == 0:
-            saved_weight = args.path2weight+"AnimeFace_alex_" + str(epoch) +".pth"
+            saved_weight = "{}AnimeFace_alex_{}.pth".format(args.path2weight, epoch)
             torch.save(model.cpu().state_dict(), saved_weight)
             model.to(device)
-    #tensorboard用のwriter閉じる
-    writer.close()
+    writer.close()#tensorboard用のwriter閉じる
     #実行時間表示
     endtime = time.time()
     interval = endtime - starttime
-    print ("elapsed time = %dh %dm %ds" % (int(interval/3600),int((interval%3600)/60),int((interval%3600)%60)))
+    print ("elapsed time = {0:d}h {1:d}m {2:d}s".format(interval/3600, (interval%3600)/60, (interval%3600)%60))
