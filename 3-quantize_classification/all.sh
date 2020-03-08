69ae54eac4ecee6e3019796a751797e98fe192c8
chatflip <oo.chat.flip@gmail.com>
2020-03-08 21:08:10 +0900

    全部やるスクリプト追加

---
+++ 3-quantize_classification/all.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+source activate pt14
+python py/main_float.py
+python py/main_dynamic_quantization.py
+python py/main_static_quantization.py
+python py/main_qat.py --batch-size=32 --epoch=20 --lr=0.00001 --lr-step-size=30 --lr-gamma=0.1
