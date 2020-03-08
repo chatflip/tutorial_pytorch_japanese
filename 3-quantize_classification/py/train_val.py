fca1e7abb04b75acd8d8e3fd4e74d8684fca2ad4
chatflip <oo.chat.flip@gmail.com>
2020-03-08 17:24:55 +0900

    inference time追加

--- 3-quantize_classification/py/train_val.py
+++ 3-quantize_classification/py/train_val.py
@@ -59,11 +59,13 @@ def validate(args, model, device, val_loader,
     # ProgressMeter, AverageMeterの値初期化
     batch_time = AverageMeter('Time', ':6.3f')
     losses = AverageMeter('Loss', ':6.5f')
+    inf_time = AverageMeter('infTime', ':8.5f')
+
     top1 = AverageMeter('Acc@1', ':6.2f')
     top5 = AverageMeter('Acc@5', ':6.2f')
     progress = ProgressMeter(
         len(val_loader),
-        [batch_time, losses, top1, top5],
+        [batch_time, inf_time, losses, top1, top5],
         prefix='Validate: ')
 
     # ネットワークを評価用に設定
@@ -75,7 +77,10 @@ def validate(args, model, device, val_loader,
         end = time.time()  # 基準の時間更新
         for i, (images, target) in enumerate(val_loader):
             images, target = images.to(device), target.to(device)  # gpu使うなら画像とラベルcuda化
+
+            inf_start = time.time()
             output = model(images)  # sofmaxm前まで出力(forward)#評価データセットでのloss計算
+            inf_time.update(time.time() - inf_start)
             loss = criterion(output, target)  # sum up batch loss
 
             # losss, accuracyを計算して更新
