14b0709fdc5bff9e021c99a4c193696531b88d08
chatflip <oo.chat.flip@gmail.com>
2020-03-08 15:00:13 +0900

    reformat

--- 3-quantize_classification/py/dynamic_quantization.py
+++ 3-quantize_classification/py/dynamic_quantization.py
@@ -15,12 +15,20 @@ from train_val import train, validate
 from utils import seed_everything
 
 
+def load_weight(model, weight_name):
+    assert os.path.isfile(weight_name), "don't exists weight: {}".format(weight_name)
+    print("use pretrained model : %s" % weight_name)
+    param = torch.load(weight_name, map_location=lambda storage, loc: storage)
+    model.load_state_dict(param)
+    return model
+
+
 if __name__ == '__main__':
     args = opt()
     print(args)
     worker_init = seed_everything(args.seed)  # 乱数テーブル固定
 
-    device = torch.device('cuda' if torch.cuda.is_available()  else 'cpu')  # cpuとgpu自動選択 (pytorch0.4.0以降の書き方)
+    device = torch.device('cpu')  # cpu only
     writer = SummaryWriter(log_dir='log/AnimeFace')  # tensorboard用のwriter作成
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
@@ -41,43 +49,38 @@ if __name__ == '__main__':
         pin_memory=False, drop_last=False,
         worker_init_fn=worker_init)
 
-    model = mobilenet_v2(pretrained=True, num_classes=args.num_classes)
-    weight_name = "weight/AnimeFace_mobilenetv2_100.pth"
-    assert os.path.isfile(weight_name), "don't exists weight: {}".format(weight_name)
-    print("use pretrained model : %s" % weight_name)
-    param = torch.load(weight_name, map_location=lambda storage, loc: storage)
-    model.load_state_dict(param)
+    model = mobilenet_v2(pretrained=False, num_classes=args.num_classes)
+    weight_name = "weight/AnimeFace_mobilenetv2_float_epoch100.pth"
+    model = load_weight(model, weight_name)
 
     quantized_model = torch.quantization.quantize_dynamic(
         model, {nn.Linear}, dtype=torch.qint8
     )
+
     saved_weight = 'weight/AnimeFace_dynamic_quantization_mobilenetv2_100.pth'
-    torch.save(quantized_model.cpu().state_dict(), saved_weight)
+    torch.save(quantized_model.state_dict(), saved_weight)
 
     optimizer = optim.SGD(
         model.parameters(), lr=args.lr, momentum=args.momentum)  # 最適化方法定義
-    criterion = nn.CrossEntropyLoss().to(device)
+    criterion = nn.CrossEntropyLoss()
     iteration = 0  # 反復回数保存用
 
     torch.set_num_threads(1)
-    starttime = time.time()  # 実行時間計測(実時間)
-    validate(args, model.to(device), device, val_loader, criterion, writer, iteration)
-    # 実行時間表示
-    endtime = time.time()
-    interval = endtime - starttime
-    print('not model elapsed time = {0:d}h {1:d}m {2:d}s'.format(
-        int(interval / 3600),
-        int((interval % 3600) / 60),
-        int((interval % 3600) % 60)))
 
     starttime = time.time()  # 実行時間計測(実時間)
-    validate(args, quantized_model.to(device), device, val_loader, criterion, writer, iteration)
+    validate(args, model, device, val_loader, criterion, writer, iteration)
     # 実行時間表示
     endtime = time.time()
     interval = endtime - starttime
-    print('quantized_model elapsed time = {0:d}h {1:d}m {2:d}s'.format(
-        int(interval / 3600),
-        int((interval % 3600) / 60),
-        int((interval % 3600) % 60)))
+    print('float model elapsed time = {0:d}m {1:d}s'.format(
+        int((interval % 3600) / 60), int((interval % 3600) % 60)))
+
+    starttime = time.time()  # 実行時間計測(実時間)
+    validate(args, quantized_model, device, val_loader, criterion, writer, iteration)
+    # 実行時間表示
+    endtime = time.time()
+    interval = endtime - starttime
+    print('dynamic quantized model elapsed time = {0:d}m {1:d}s'.format(
+        int((interval % 3600) / 60), int((interval % 3600) % 60)))
 
     writer.close()  # tensorboard用のwriter閉じる
