20e8fafbe0b9acea0e8bc257c34f7be6c7d89caf
chatflip <oo.chat.flip@gmail.com>
2020-06-13 17:37:09 +0900

    apexと分散学習追加

--- 5-segment_Penn-Fudan/py/main.py
+++ 5-segment_Penn-Fudan/py/main.py
@@ -4,70 +4,108 @@ import torch
 import torch.nn as nn
 
 import utils
+from args import opt
 from engine import train_one_epoch, evaluate
 from transform import get_transform
 from dataset import PennFudanDataset
 from model import get_instance_segmentation_model
 
+try:
+    from apex import amp
+except ImportError:
+    amp = None
+
+
+def load_data(args):
+    # use our dataset and defined transformations
+    train_dataset = PennFudanDataset(
+        os.path.join(args.path2db, 'PennFudanPed'), get_transform(train=True))
+    val_dataset = PennFudanDataset(
+        os.path.join(args.path2db, 'PennFudanPed'), get_transform(train=False))
+
+    if args.distributed:
+        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+        val_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset)
+    else:
+        train_sampler = torch.utils.data.RandomSampler(train_dataset)
+        val_sampler = torch.utils.data.SequentialSampler(val_dataset)
+    return train_dataset, val_dataset, train_sampler, val_sampler
+
 
 def main():
+    args = opt()
+    print(args)
+    utils.seed_everything(args.seed)  # 乱数テーブル固定
+    collate_fn = utils.get_worker_init()
+    if args.apex and amp is None:
+        raise RuntimeError("Failed to import apex. Please install apex from https://www.github.com/nvidia/apex "
+                           "to enable mixed-precision training.")
     # フォルダが存在してなければ作る
     if not os.path.exists('weight'):
         os.mkdir('weight')
 
-    # use our dataset and defined transformations
-    dataset = PennFudanDataset('data/PennFudanPed', get_transform(train=True))
-    dataset_test = PennFudanDataset('data/PennFudanPed', get_transform(train=False))
+    utils.init_distributed_mode(args)
+    ngpus_per_node = torch.cuda.device_count()
 
-    # split the dataset in train and test set
-    torch.manual_seed(1)
+    if args.distributed:
+        args.batch_size = int(args.batch_size / ngpus_per_node)
+        args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
+
+    train_dataset, val_dataset, train_sampler, val_sampler = load_data(args)
 
     # define training and validation data loaders
-    data_loader = torch.utils.data.DataLoader(
-        dataset, batch_size=4, shuffle=True, num_workers=4,
-        collate_fn=utils.collate_fn)
+    train_loader = torch.utils.data.DataLoader(
+        dataset=train_dataset, batch_size=args.batch_size,
+        sampler = train_sampler, num_workers=args.workers,
+        pin_memory=True, drop_last=True,
+        collate_fn=collate_fn)
 
-    data_loader_test = torch.utils.data.DataLoader(
-        dataset_test, batch_size=2, shuffle=False, num_workers=4,
-        collate_fn=utils.collate_fn)
+    val_loader = torch.utils.data.DataLoader(
+        dataset=val_dataset, batch_size=args.batch_size,
+        sampler=val_sampler, num_workers=args.workers,
+        pin_memory=True, drop_last=False,
+        collate_fn=collate_fn)
+
 
     device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
-    multigpu = torch.cuda.device_count() > 1  # グラボ2つ以上ならmultigpuにする
-
-    # our dataset has two classes only - background and person
-    num_classes = 2
 
     # get the model using our helper function
-    model = get_instance_segmentation_model(num_classes)
+    model = get_instance_segmentation_model(args.num_classes)
     # move model to the right device
     model.to(device)
 
-
     # construct an optimizer
     params = [p for p in model.parameters() if p.requires_grad]
-    optimizer = torch.optim.SGD(params, lr=0.005,
-                                momentum=0.9, weight_decay=0.0005)
+    optimizer = torch.optim.SGD(
+        params, lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
 
-    # and a learning rate scheduler which decreases the learning rate by
-    # 10x every 3 epochs
-    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
-                                                   step_size=3,
-                                                   gamma=0.1)
+
+    # --evaluate
+    # --resume
+
+    if args.apex:
+        model, optimizer = amp.initialize(
+            model, optimizer,
+            opt_level=args.apex_opt_level
+        )
+
     model_without_ddp = model
-    if multigpu:
-        model = nn.DataParallel(model)
+    if args.distributed:
+        model = nn.parallel.DistributedDataParallel(model, device_ids=[args.gpu])
         model_without_ddp = model.module
 
-    # let's train it for 10 epochs
-    num_epochs = 10
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, step_size=args.lr_step_size, gamma=args.lr_gamma)
 
-    for epoch in range(num_epochs):
+    for epoch in range(args.epochs):
+        if args.distributed:
+            train_sampler.set_epoch(epoch)
         # train for one epoch, printing every 10 iterations
-        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        train_one_epoch(model, optimizer, train_loader, device, epoch, print_freq=args.print_freq)
         # update the learning rate
         lr_scheduler.step()
         # evaluate on the test dataset
-        evaluate(model, data_loader_test, device=device)
+        evaluate(model, val_loader, device=device)
 
         saved_weight = 'weight/Penn-Fudan_maskrcnn_epoch{}.pth'.format(epoch)
         torch.save(model_without_ddp.cpu().state_dict(), saved_weight)
