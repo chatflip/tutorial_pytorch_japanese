cf1f1ac1984546a42e73ea11f0df4e839e35d67e
chatflip <oo.chat.flip@gmail.com>
2020-06-13 18:35:14 +0900

    apex対応

--- 5-segment_Penn-Fudan/py/engine.py
+++ 5-segment_Penn-Fudan/py/engine.py
@@ -9,6 +9,11 @@ from coco_utils import get_coco_api_from_dataset
 from coco_eval import CocoEvaluator
 import utils
 
+try:
+    from apex import amp
+except ImportError:
+    amp = None
+
 
 def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
     model.train()
@@ -43,7 +48,11 @@ def train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq):
             sys.exit(1)
 
         optimizer.zero_grad()
-        losses.backward()
+        if apex:
+            with amp.scale_loss(losses, optimizer) as scaled_loss:
+                scaled_loss.backward()
+        else:
+            losses.backward()
         optimizer.step()
 
         if lr_scheduler is not None:
