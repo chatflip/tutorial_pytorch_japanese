f7db2c785d573f86272cf7af76f85973d18ac428
chatflip <oo.chat.flip@gmail.com>
2020-09-17 15:59:37 +0900

    リニューアルのため全削除

--- 4-quantize_classification/all.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-source activate pt15
-python py/main_float.py --apex
-python py/main_dynamic_quantization.py
-python py/main_static_quantization.py --batch-size=32 --backend=fbgemm
-python py/main_qat.py --batch-size=64 --val-batch-size=128 --epoch=20 --lr=0.00001 --lr-step-size=30 --lr-gamma=0.1
-python py/main_validate.py --val-batch-size=1 --print-freq=1000
--- 5-segment_Penn-Fudan/py/transform.py
+++
@@ -1,9 +0,0 @@
-import transforms as T
-
-
-def get_transform(train):
-    transforms = []
-    transforms.append(T.ToTensor())
-    if train:
-        transforms.append(T.RandomHorizontalFlip(0.5))
-    return T.Compose(transforms)
