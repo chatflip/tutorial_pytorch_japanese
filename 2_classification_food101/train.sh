8f9f05f71f4ad4ba984a79db6b8a0902ea66c006
chatflip <oo.chat.flip@gmail.com>
2021-03-31 01:19:05 +0900

    実行arch変更

--- 2_classification_food101/train.sh
+++ 2_classification_food101/train.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # poetry shell
-python py/main.py -m apex=true arch=mobilenet_v2,resnet50
-#python py/main.py -m arch=resnet50 arch=efficientnet_b0,efficientnet_b1
+python py/main.py -m apex=true arch=mobilenet_v2 epochs=10
+python py/main.py -m arch=efficientnet_b0,efficientnet_b3,efficientnet_b5,efficientnet_b7 epochs=3
